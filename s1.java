7ed50311b6946eb956667e8aaa28a367421b03ec
Sreejit1234 <190206@kiit.ac.in>
2020-12-26 11:11:41 +0530

    Commit 1

--- s1.java
+++ s1.java
@@ -2,4 +2,7 @@ import java.util.*;
 public static void main(String args[])
 {
   System.out.println("Hello World.");
+  System.out.println("Enter a number:- ");
+  int x=sc.nextInt();
+  System.out.println("X = "+x);
 }
