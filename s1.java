5210b70f65f240732415b908f37f70e26f82acb0
Sreejit Das <69572549+Sreejit1234@users.noreply.github.com>
2020-12-26 11:15:22 +0530

    commit2

--- s1.java
+++ s1.java
@@ -2,7 +2,8 @@ import java.util.*;
 public static void main(String args[])
 {
   System.out.println("Hello World.");
-  System.out.println("Enter a number:- ");
+  System.out.println("Enter 2 numbers:- ");
   int x=sc.nextInt();
-  System.out.println("X = "+x);
+  int y=sc.nextInt();
+  System.out.println("X = "+x"  Y = "+y);
 }
